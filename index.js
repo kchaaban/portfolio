186befaf570242c39020c3db2d342d569edd8809
Khaled Chaaban <khaledchaaban@example.com>
2025-04-19 16:00:05 +0300

    Deploying to GitHub Pages

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/portfolio/",
+  // <--- important for GitHub Pages
   plugins: [
     react(),
     runtimeErrorOverlay(),
